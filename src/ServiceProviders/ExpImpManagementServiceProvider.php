e4ff3f7d0425c63980108535bc1b95a16a327c14
Muhammed ALDRUBI <MuhammedALDRUBI>
2024-10-19 20:20:07 +0300

    test changing

--- src/ServiceProviders/ExpImpManagementServiceProvider.php
+++ src/ServiceProviders/ExpImpManagementServiceProvider.php
@@ -9,6 +9,7 @@ class ExpImpManagementServiceProvider extends ServiceProvider
 
     public function boot()
     {
+        //test changing
         // $this->publishes(
             // [__DIR__ . "/../../config/validator-lib-config.php" => config_path("validator-lib-config.php") ] ,
             // 'validator-lib-config'
